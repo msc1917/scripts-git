07076edde6a872836fd9bbe10a7a7ca6e2989fc2
Martin Schatz <martin.schatz@gmail.com>
2023-01-02 23:30:30 +0100

    Script to generate a list of repositories

--- build_git_list.sh
+++ build_git_list.sh
@@ -1,17 +1,66 @@
 #! /bin/sh
 list_directory=repository_list
-list_file=repositories.txt
+list_file_markup=repositories.md
+list_file_json=repositories.json
+
+
+# ------------------------------- | ------------------------------- | ------------------------------- | -------------------------------
+# assignableUsers                 | codeOfConduct                   | contactLinks                    | createdAt
+# defaultBranchRef                | deleteBranchOnMerge             | description                     | diskUsage
+# forkCount                       | fundingLinks                    | hasIssuesEnabled                | hasProjectsEnabled
+# hasWikiEnabled                  | homepageUrl                     | id                              | isArchived 
+# isBlankIssuesEnabled            | isEmpty                         | isFork                          | isInOrganization 
+# isMirror                        | isPrivate                       | isSecurityPolicyEnabled         | isTemplate
+# isUserConfigurationRepository   | issueTemplates                  | issues                          | labels
+# languages                       | latestRelease                   | licenseInfo                     | mentionableUsers
+# mergeCommitAllowed              | milestones                      | mirrorUrl                       | name
+# nameWithOwner                   | openGraphImageUrl               | owner                           | parent
+# primaryLanguage                 | projects                        | pullRequestTemplates            | pullRequests
+# pushedAt                        | rebaseMergeAllowed              | repositoryTopics                | securityPolicyUrl
+# squashMergeAllowed              | sshUrl                          | stargazerCount                  | templateRepository
+# updatedAt                       | url                             | usesCustomOpenGraphImage        | viewerCanAdminister
+# viewerDefaultCommitEmail        | viewerDefaultMergeMethod        | viewerHasStarred                | viewerPermission
+# viewerPossibleCommitEmails      | viewerSubscription              | watchers                        |
+# ------------------------------- | ------------------------------- | ------------------------------- | -------------------------------
+
+gh repo list --json name,isPrivate -L 200 > ${list_directory}/${list_file_json}
+repository_name_length=0
+
+for repository in $(cat ${list_directory}/${list_file_json} | jq '.[].name')
+do
+	# echo "${repository} ... ${#repository}/${repository_name_length}"
+	if [ ${repository_name_length} -le ${#repository} ]
+	then
+		repository_name_length=${#repository}
+	fi
+done
+
+# repository_name_length=${repository_name_length} + 1
 
 if [ ! -d ${list_directory} ]
 then
+	echo "Erstelle ${list_directory}."
 	mkdir ${list_directory}
 fi
 
-for repository in $(gh repo list -L 200 | cut -f 1)
+if [ ! -f ${list_directory}/${list_file_markup} ]
+then
+	printf "%-${repository_name_length}s | %-10s | %-20s \n" "Repository" "Modus" "Beschreibung" >> ${list_directory}/${list_file_markup}
+	printf "%-${repository_name_length}s | %-10s | %-20s \n" "---" "---" "---" >> ${list_directory}/${list_file_markup}
+fi
+
+for repository in $(cat ${list_directory}/${list_file_json} | jq -r '.[].name')
 do
-	if grep "^${repository} " ${list_directory}/${list_file}
+	if grep -q "^${repository} *|" ${list_directory}/${list_file_markup}
 	then
-		echo "${repository} ist in ${list_directory}/${list_file}"
+		echo "${repository} ist in ${list_directory}/${list_file_markup}"
 	else
-		echo "${repository}    " >> ${list_directory}/${list_file}
+		if $(cat ${list_directory}/${list_file_json} | jq -r ".[]|select(.name==\"${repository}\").isPrivate")
+		then
+			repository_mode="Private"
+		else
+			repository_mode="Public"
+		fi
+		printf "%-${repository_name_length}s | %-10s | \n" "${repository}" "${repository_mode}" >> ${list_directory}/${list_file_markup}
 	fi
+done
