77600ca211041b2421ee88077f697351523a395b
Martin Schatz <martin.schatz@gmail.com>
2023-01-04 21:56:03 +0100

    added check for submodule-detached-check

--- check-detached.sh
+++ check-detached.sh
@@ -2,7 +2,7 @@
 
 module_directory=~/script/ansible/roles
 directory_length=0
-ls ${module_directory} | while read directory;
+for directory in $(ls ${module_directory});
 do
 	full_directory="${module_directory}/${directory}"
 	if [ ${#full_directory} -gt ${directory_length} ]
@@ -13,31 +13,36 @@ done
 
 ls ${module_directory} | while read directory;
 do
-	cd ${module_directory}/${directory}
-	result=$(git status)
-	if echo ${result} | grep -q "HEAD detached at"
+	if [ -d ${module_directory}/${directory} -a -f ${module_directory}/${directory}/.git ]
 	then
-		git_detached=1
-	else
-		git_detached=0
-	fi
-	if echo ${result} | grep -q "nothing to commit, working tree clean"
-	then
-		git_clean=1
-	else
-		git_clean=0
-	fi
+		cd ${module_directory}/${directory}
+		result=$(git status)
+		if echo ${result} | grep -q "HEAD detached at"
+		then
+			git_detached=1
+		else
+			git_detached=0
+		fi
+		if echo ${result} | grep -q "nothing to commit, working tree clean"
+		then
+			git_clean=1
+		else
+			git_clean=0
+		fi
 
-	if [ ${git_detached} -eq 1 -a ${git_clean} -eq 1 ]
-	then
-		printf "%${directory_length}s: %1s\n" "${module_directory}/${directory}" "Detached head and clean repository: fixing..."
-		git checkout main
-	elif [ ${git_detached} -eq 1 -a ${git_clean} -eq 0 ]
-	then
-		printf "%${directory_length}s: %1s\n" "${module_directory}/${directory}" "Detached head but no clean repository: manual fix needed..."
-	else
-		printf "%${directory_length}s: %1s\n" "${module_directory}/${directory}" "Clean Git repository, nothing to do..."
-	fi
+		if [ ${git_detached} -eq 1 -a ${git_clean} -eq 1 ]
+		then
+			printf "%${directory_length}s: %1s\n" "${module_directory}/${directory}" "Detached head and clean repository: fixing..."
+			git checkout main
+		elif [ ${git_detached} -eq 1 -a ${git_clean} -eq 0 ]
+		then
+			printf "%${directory_length}s: %1s\n" "${module_directory}/${directory}" "Detached head but no clean repository: manual fix needed..."
+		else
+			printf "%${directory_length}s: %1s\n" "${module_directory}/${directory}" "Clean Git repository, nothing to do..."
+		fi
 
-	cd - >/dev/null
+		cd - >/dev/null
+	else
+		printf "%${directory_length}s: %1s\n" "${module_directory}/${directory}" "No GIT-Repository"
+	fi
 done
