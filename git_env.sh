cfabd8c3656cdded170ec343c7e3bd7757a31d9a
Martin Schatz <martin.schatz@itsv.at>
2023-02-07 13:05:48 +0100

    git_env.sh verfollständigt, task aber noch deaktiviert

--- git_env.sh
+++ git_env.sh
@@ -23,15 +23,34 @@ do
 find ${default_config_dir}/git_list.d/ -type f -name *.list | while read filename;
 do cat ${filename} | grep -v "^ *$" | while read LINE;
 	do
+		ignore=true
 		if echo ${LINE} | grep -q "^\[[^]][^]]*\].*"
 		then
 			git_category="$(echo ${LINE} | sed "s/^\[\([^]][^]]*\)\].*$/\1/g")"
-			echo "Kathegorie: >${git_category}<"
+			if echo "${git_category}" |grep -q ${1}
+			then
+				ignore=false
+				echo "Kathegorie: >${git_category}<"
+			else
+				ignore=true
+				echo "Kathegorie: \"${git_category}\" (Wird ignoriert)"
+			fi
 		else
 			github_address=$(echo ${LINE} | cut -f 1 -d " ")
 			target_path=$(echo ${LINE} | cut -f 2 -d " ")
-			git_branch=$(echo ${LINE} | cut -f 3 -d " ")
-			echo "  ==> ${github_address}[${git_branch}] -> ${target_path}"
+			git_branch="["$(echo ${LINE} | cut -f 3 -d " ")"]"
+			if ${ignore}
+			then
+				echo "  ==> ${github_address}${git_branch} -> ${target_path} (Wird ignoriert)"
+			else
+				if [ ! -d "${target_path}" ]
+				then
+					echo "  ==> ${github_address}${git_branch} -> ${target_path} (Klone Repository)"
+					# git clone ${github_address}${git_branch} ${target_path}
+				else
+					echo "  ==> ${github_address}${git_branch} -> ${target_path} (Repository bereits Vorhanden)"
+				fi
+			fi
 		fi
 	done
 done
