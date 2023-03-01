18fcae07430bd2b408ac3c7c418b53a7e90517e4
Martin Schatz <martin.schatz@gmail.com>
2023-03-01 08:57:07 +0100

    git_env activated

--- git_env.sh
+++ git_env.sh
@@ -20,25 +20,37 @@ do
 
 
 
-find ${default_config_dir}/git_list.d/ -type f -name *.list | while read filename;
-do cat ${filename} | grep -v "^ *$" | while read LINE;
+# find ${default_config_dir}/git_list.d/ -type f -name *.list | while read filename;
+for filename in $(find ${default_config_dir}/git_list.d/ -type f -name *.list)
+do
+ignore=true
+	cat ${filename} | grep -v "^ *$" | grep -v "^ *#" | while read LINE;
 	do
-		ignore=true
-		if echo ${LINE} | grep -q "^\[[^]][^]]*\].*"
+		if echo ${LINE} | grep -q "^ *\[[^]][^]]*\].*"
 		then
-			git_category="$(echo ${LINE} | sed "s/^\[\([^]][^]]*\)\].*$/\1/g")"
-			if echo "${git_category}" |grep -q ${1}
+			git_category="$(echo "${LINE}" | sed "s/^ *\[\([^]][^]]*\)\].*$/\1/g")"
+			if [ "${1}" != "" ]
 			then
-				ignore=false
-				echo "Kathegorie: >${git_category}<"
+				if echo "${git_category}" |grep -q ${1}
+				then
+					ignore=false
+					echo "Kathegorie: \"${git_category}\""
+				else
+					ignore=true
+					echo "Kathegorie: \"${git_category}\" (Wird ignoriert)"
+				fi
 			else
-				ignore=true
-				echo "Kathegorie: \"${git_category}\" (Wird ignoriert)"
+				ignore=false
+				echo "Kathegorie: \"${git_category}\""
 			fi
 		else
 			github_address=$(echo ${LINE} | cut -f 1 -d " ")
 			target_path=$(echo ${LINE} | cut -f 2 -d " ")
-			git_branch="["$(echo ${LINE} | cut -f 3 -d " ")"]"
+			git_branch="$(echo ${LINE} | cut -f 3 -d " ")"
+			if [ "${git_branch}" != "" ]
+			then
+				git_branch="[${git_branch}]"
+			fi
 			if ${ignore}
 			then
 				echo "  ==> ${github_address}${git_branch} -> ${target_path} (Wird ignoriert)"
@@ -46,7 +58,7 @@ do cat ${filename} | grep -v "^ *$" | while read LINE;
 				if [ ! -d "${target_path}" ]
 				then
 					echo "  ==> ${github_address}${git_branch} -> ${target_path} (Klone Repository)"
-					# git clone ${github_address}${git_branch} ${target_path}
+					git clone ${github_address}${git_branch} ${target_path}
 				else
 					echo "  ==> ${github_address}${git_branch} -> ${target_path} (Repository bereits Vorhanden)"
 				fi
@@ -54,3 +66,5 @@ do cat ${filename} | grep -v "^ *$" | while read LINE;
 		fi
 	done
 done
+
+echo "Fertig"
