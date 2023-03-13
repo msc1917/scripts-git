40df5aec34ac405188584b4454fbcd5b6a391fdc
Martin Schatz <martin.schatz@gmail.com>
2023-03-13 16:57:24 +0100

    Color to git_env.sh

--- git_env.sh
+++ git_env.sh
@@ -25,29 +25,39 @@ intend()
 {
 	while read LINE;
 	do
-		echo "      ${LINE}"
+		echo "${col_act}      ${LINE}${col_off}"
 	done
 }
 
-echo "\nPreparing..."
+if which tput > /dev/null
+then
+	echo "tput-mode"
+	export col_act=$(tput setaf 3)
+	export col_inact=$(tput setaf 6)
+	export col_err=$(tput setaf 1)
+	export col_bold=$(tput bold)
+	export col_off=$(tput sgr0)
+fi
+
+printf "${col_act}${col_bold}\nPreparing...\n${col_off}"
 if [ ! -d ${default_config_dir}/git_list.d ]
 then
-	echo "  ==> Getting settings from Github (${git_settings_repo})"
+	printf "${col_act}  ==> Getting settings from Github (${git_settings_repo})\n${col_off}"
 	git clone ${git_settings_repo} ${default_config_dir}/git_list.d 2>&1 | intend
 else
 	if [ -d ${default_config_dir}/git_list.d/.git -a -f ${default_config_dir}/git_list.d/.git/FETCH_HEAD ]
 	then
 		if [ $(expr $(date "+%s") - 600) -gt $(stat -c %Y ${default_config_dir}/git_list.d/.git/FETCH_HEAD) ]
 		then
-			echo "  ==> Actualize settings from Github (${git_settings_repo})"
+			printf "${col_act}  ==> Actualize settings from Github (${git_settings_repo})${col_off}\n"
 			git -C ${default_config_dir}/git_list.d push 2>&1 | intend
 			git -C ${default_config_dir}/git_list.d pull 2>&1 | intend
 			touch ${default_config_dir}/git_list.d/.git/FETCH_HEAD
 		else
-			echo "  ==> Last pull least 10 Min ago Github (${git_settings_repo})"
+			echo "${col_inact}  ==> Last pull least 10 Min ago Github (${git_settings_repo})${col_off}"
 		fi
 	else
-		echo "  ==> [Error]: ${default_config_dir}/git_list.d seems to be no git repository for ${git_settings_repo}"
+		echo "${col_err}  ==> [Error]: ${default_config_dir}/git_list.d seems to be no git repository for ${git_settings_repo}${col_off}"
 	fi
 fi
 
@@ -74,14 +84,14 @@ ignore=true
 				if ${line_in_parameters}
 				then
 					ignore=false
-					echo "\nKathegorie: \"${git_category}\""
+					echo "${col_act}${col_bold}\nKathegorie: \"${git_category}\"${col_off}"
 				else
 					ignore=true
-					echo "\nKathegorie: \"${git_category}\" (Wird ignoriert)"
+					echo "${col_inact}${col_bold}\nKathegorie: \"${git_category}\" (Wird ignoriert)${col_off}"
 				fi
 			else
 				ignore=false
-				echo "\nKathegorie: \"${git_category}\""
+				echo "${col_act}${col_bold}\nKathegorie: \"${git_category}\"${col_off}"
 			fi
 		else
 			git_branch=[]
@@ -94,33 +104,33 @@ ignore=true
 			fi
 			if ${ignore}
 			then
-				echo "  ==> ${github_address}${git_branch} -> ${target_path} (Wird ignoriert)"
+				echo "${col_inact}  ==> ${github_address}${git_branch} -> ${target_path} (Wird ignoriert)${col_off}"
 			else
 				if [ ! -d "${target_path}" ]
 				then
 					if [ -d $(echo "${target_path}" | sed "s#^~#${HOME}#") ]
 					then
-						echo "  ==> ${github_address}${git_branch} -> ${target_path} (Pull Repository)"
+						echo "${col_act}  ==> ${github_address}${git_branch} -> ${target_path} (Pull Repository)${col_off}"
 						git -C $(echo "${target_path}" | sed "s#^~#${HOME}#") pull 2>&1 | intend
-						
+
 						if echo "${my_git_repos}" | grep -q "$(echo ${github_address} | sed "s#^[^@][^@]*@\([^/][^/]*\)/.*#\1#")"
 						then
 							git -C $(echo "${target_path}" | sed "s#^~#${HOME}#") push 2>&1 | intend
 						else
-							echo "No automatic push from ${github_address}..." | intend
+							echo "${col_noact}No automatic push from ${github_address}...${col_off}" | intend
 						fi
 					else
-						echo "  ==> ${github_address}${git_branch} -> ${target_path} (Klone Repository)"
+						echo "${col_act}  ==> ${github_address}${git_branch} -> ${target_path} (Klone Repository)${col_off}"
 						git clone ${github_address} $(echo "${target_path}" | sed "s#^~#${HOME}#") 2>&1 | intend
 					fi
 
 					# git clone ${github_address}${git_branch} $(echo "${target_path}" | sed "s#^~#${HOME}#")
 				else
-					echo "  ==> ${github_address}${git_branch} -> ${target_path} (Repository bereits Vorhanden)"
+					echo "${col_inact}  ==> ${github_address}${git_branch} -> ${target_path} (Repository bereits Vorhanden)${col_off}"
 				fi
 			fi
 		fi
 	done
 done
 
-echo "Fertig"
+echo "${col_act}${col_bold}Fertig${col_off}"
